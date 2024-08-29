daf8cbcd5df9b1c3af0aab24d72237cf3ef5f5df
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-29 22:21:02 +0300

    Lab1 ex_3 : navigation styles

---
+++ Lab1/ex_3/script/script.js
@@ -0,0 +1,15 @@
+document.getElementById("up").addEventListener("click", () => {
+  console.log("up");
+});
+
+document.getElementById("down").addEventListener("click", () => {
+  console.log("down");
+});
+
+document.getElementById("right").addEventListener("click", () => {
+  console.log("right");
+});
+
+document.getElementById("left").addEventListener("click", () => {
+  console.log("left");
+});
