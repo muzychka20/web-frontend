42450dd4d54a44bb62df5c62d440ad850e1b805f
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-30 23:02:47 +0300

    Lab1 ex_3: added canvas with logic for it

--- Lab1/ex_3/script/script.js
+++ Lab1/ex_3/script/script.js
@@ -1,15 +1,41 @@
+let x = 70;
+let y = 70;
+let difference = 50;
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.canvas.width = 500;
+ctx.canvas.height = 500;
+
+ctx.beginPath();
+ctx.moveTo(x, y);
+
+function drawLine(x, y) {
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
 document.getElementById("up").addEventListener("click", () => {
   console.log("up");
+  y -= difference;
+  drawLine(x, y);
 });
 
 document.getElementById("down").addEventListener("click", () => {
   console.log("down");
+  y += difference;
+  drawLine(x, y);
 });
 
 document.getElementById("right").addEventListener("click", () => {
   console.log("right");
+  x += difference;
+  drawLine(x, y);
 });
 
 document.getElementById("left").addEventListener("click", () => {
   console.log("left");
+  x -= difference;
+  drawLine(x, y);
 });
