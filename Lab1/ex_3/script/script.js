8d5458f2540462fa9da8d4d5f0a31cd8202607fd
KyryloMuzychka <kyryll2016@gmail.com>
2024-09-01 16:56:14 +0300

    added lab1_ex_3

--- Lab1/ex_3/script/script.js
+++ Lab1/ex_3/script/script.js
@@ -1,6 +1,6 @@
-let x = 70;
-let y = 70;
-let difference = 50;
+let x = parseInt(prompt("Start x ="));
+let y = parseInt(prompt("Start y = "));
+let difference = parseInt(prompt("Step = "));
 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
@@ -8,34 +8,63 @@ let ctx = canvas.getContext("2d");
 ctx.canvas.width = 500;
 ctx.canvas.height = 500;
 
-ctx.beginPath();
+// Set the line properties
+ctx.lineWidth = 5;
+ctx.lineCap = "round";
+ctx.strokeStyle = "rgb(60, 131, 255)";
+
 ctx.moveTo(x, y);
 
+drawPoint(x, y);
+
+// Draw the point
+function drawPoint(x, y) {
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, 2 * Math.PI); // Draw a circle to mark the point
+  ctx.fill();
+}
+
+// Draw the line
 function drawLine(x, y) {
   ctx.lineTo(x, y);
   ctx.stroke();
 }
 
-document.getElementById("up").addEventListener("click", () => {
-  console.log("up");
-  y -= difference;
+// Draw the line with point at the end
+function draw(x, y) {
   drawLine(x, y);
+  drawPoint(x, y);
+}
+
+document.getElementById("up").addEventListener("click", () => {
+  if (y - difference > 0) {
+    ctx.moveTo(x, y - 10);
+    y -= difference;
+    draw(x, y);
+  }
 });
 
 document.getElementById("down").addEventListener("click", () => {
-  console.log("down");
-  y += difference;
-  drawLine(x, y);
+  if (y + difference < ctx.canvas.height) {
+    ctx.moveTo(x, y + 10);
+    y += difference;
+    draw(x, y);
+  }
 });
 
 document.getElementById("right").addEventListener("click", () => {
-  console.log("right");
-  x += difference;
-  drawLine(x, y);
+  if (x + difference < ctx.canvas.width) {
+    ctx.moveTo(x + 10, y);
+    x += difference;
+    draw(x, y);
+  }
 });
 
 document.getElementById("left").addEventListener("click", () => {
-  console.log("left");
-  x -= difference;
-  drawLine(x, y);
+  if (x - difference > 0) {
+    ctx.moveTo(x - 10, y);
+    x -= difference;
+    draw(x, y);
+  }
 });
